34f55dff0d1e1f7a9f706f7ee21752beac937dc9
arloor <admin@arloor.com>
2020-08-01 22:41:03 +0800

    init

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer');
 async function main() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://github.com');
+    await page.goto('https://arloor.com');
     await page.screenshot({path: 'example.png'});
 
     await browser.close();

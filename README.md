62cafa4e34867bb9e6264df90874f930425c0149
arloor <admin@arloor.com>
2020-08-01 22:36:48 +0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# puppeteer使用
+
+版本： puppeteer v5.2.1
+
+puppeteer文档： https://pptr.dev/#?product=Puppeteer&version=v5.2.1
+
+## node使用
+
+1. npm init
+2. npm install
+
+可以设置http_proxy、https_proxy环境变量以设置npm代理，以及加速内置的chromium下载。
+
+知道node的async和await我就开始写。
+
+
+
---
+++ index.js
@@ -0,0 +1,13 @@
+// puppeteer文档： https://pptr.dev/#?product=Puppeteer&version=v5.2.1
+const puppeteer = require('puppeteer');
+
+async function main() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://github.com');
+    await page.screenshot({path: 'example.png'});
+
+    await browser.close();
+};
+
+main();
